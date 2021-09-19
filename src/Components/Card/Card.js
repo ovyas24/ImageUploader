f536baff105b54ceb6831abd56bb5d6afa06b14f
ovyas24 <ovyas24@gmail.com>
2021-09-19 12:44:06 +0530

    first commit

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,13 @@
+import ImageUploader from '../Imageuploader/Imageuplaoder';
+import './Card.css';
+
+
+function Card() {
+  return (
+    <div className="Card">
+      <ImageUploader />
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/Components/Imageuploader/Imageuplaoder.js
@@ -0,0 +1,11 @@
+import "./imageUploader.css"
+
+const ImageUploader = () => {
+    return (
+        <div>
+            <h4 className="upload-image">Upload your Image</h4>
+        </div>
+    )
+}
+
+export default ImageUploader;
